61331c80e8987a346c2d0c0e198839c76d329663
Devang Mundhra <devangmundhra@gmail.com>
2014-10-13 01:39:55 -0700

    celerybeat fix again

--- alltoez/configs/production/celerybeat.sh
+++ alltoez/configs/production/celerybeat.sh
@@ -3,8 +3,10 @@ set -e
 LOGFILE=/home/django/logs/beat.log
 ERRORFILE=/home/django/logs/beat.log
 LOGDIR=$(dirname $LOGFILE)
-PIDFILE=/home/celery/pidfile
+PIDFILE=/home/celery/celery.pidfile
 PIDDIR=$(dirname $PIDFILE)
+SCHEDFILE=/home/celery/celerybeat_sched
+SCHEDDIR=$(dirname $SCHEDFILE)
 
 #we don't want to run this as root..
 USER=celery
@@ -17,4 +19,6 @@ cd /home/django/sites/alltoez/repository/alltoez/configs/production
 test -d $LOGDIR || mkdir -p $LOGDIR
 test -d $PIDDIR || mkdir -p $PIDDIR
 sudo chown $USER:$GROUP $PIDDIR
-exec /home/django/sites/alltoez/env/bin/celery beat -A celeryapp --loglevel=INFO --pidfile=$PIDFILE
+test -d SCHEDDIR || mkdir -p SCHEDDIR
+sudo chown $USER:$GROUP SCHEDDIR
+exec /home/django/sites/alltoez/env/bin/celery beat -A celeryapp --loglevel=INFO --pidfile=$PIDFILE --schedule=$SCHEDFILE
