36e188d4abc950c87beb65bcfb48f86c19ab71c6
Devang Mundhra <devangmundhra@gmail.com>
2015-01-09 10:12:41 -0800

    Add newrelic

--- alltoez/configs/production/wsgi.py
+++ alltoez/configs/production/wsgi.py
@@ -1,7 +1,9 @@
 import os
-
 # put the Django project on sys.path
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "alltoez.configs.production..settings")
+
+import newrelic.agent
+newrelic.agent.initialize('newrelic.ini', 'production')
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
