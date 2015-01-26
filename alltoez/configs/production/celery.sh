69f31632925b279c237a8eeb7bf3842991ed38da
Devang Mundhra <devangmundhra@gmail.com>
2015-01-26 00:19:51 -0800

    Run celery with new relic too

--- alltoez/configs/production/celery.sh
+++ alltoez/configs/production/celery.sh
@@ -4,9 +4,14 @@ LOGFILE=/home/django/logs/worker.log
 ERRORFILE=/home/django/logs/worker.log
 LOGDIR=$(dirname $LOGFILE)
 
+NEW_RELIC_CONFIG_FILE=/home/django/sites/alltoez/repository/alltoez/configs/production/newrelic.ini
+export NEW_RELIC_CONFIG_FILE
+NEW_RELIC_ENVIRONMENT=production
+export NEW_RELIC_ENVIRONMENT
+
 cd /home/django/sites/alltoez/repository
 source ../env/bin/activate
 export PYTHONPATH=$PYTHONPATH:/home/django/sites/alltoez/repository/alltoez
 cd /home/django/sites/alltoez/repository/alltoez/configs/production
 test -d $LOGDIR || mkdir -p $LOGDIR
-exec /home/django/sites/alltoez/env/bin/celery worker -A celeryapp --loglevel=INFO
+newrelic-admin run-program /home/django/sites/alltoez/env/bin/celery worker -A celeryapp --loglevel=INFO
--- alltoez/configs/production/celerybeat.sh
+++ alltoez/configs/production/celerybeat.sh
@@ -8,6 +8,11 @@ PIDDIR=$(dirname $PIDFILE)
 SCHEDFILE=/home/celery/celerybeat_sched
 SCHEDDIR=$(dirname $SCHEDFILE)
 
+NEW_RELIC_CONFIG_FILE=/home/django/sites/alltoez/repository/alltoez/configs/production/newrelic.ini
+export NEW_RELIC_CONFIG_FILE
+NEW_RELIC_ENVIRONMENT=production
+export NEW_RELIC_ENVIRONMENT
+
 cd /home/django/sites/alltoez/repository
 source ../env/bin/activate
 export PYTHONPATH=$PYTHONPATH:/home/django/sites/alltoez/repository/alltoez
@@ -15,4 +20,4 @@ cd /home/django/sites/alltoez/repository/alltoez/configs/production
 test -d $LOGDIR || mkdir -p $LOGDIR
 test -d $PIDDIR || mkdir -p $PIDDIR
 test -d $SCHEDDIR || mkdir -p $SCHEDDIR
-exec /home/django/sites/alltoez/env/bin/celery beat -A celeryapp --loglevel=INFO --pidfile=$PIDFILE --schedule=$SCHEDFILE
+newrelic-admin run-program /home/django/sites/alltoez/env/bin/celery beat -A celeryapp --loglevel=INFO --pidfile=$PIDFILE --schedule=$SCHEDFILE
