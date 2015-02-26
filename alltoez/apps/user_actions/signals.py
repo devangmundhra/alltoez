93bcd8abce69e7a44b7f771430173c157980a3fe
Devang Mundhra <devangmundhra@gmail.com>
2015-02-25 23:53:21 -0800

    Another try with custom metrics

--- alltoez/apps/user_actions/signals.py
+++ alltoez/apps/user_actions/signals.py
@@ -4,43 +4,25 @@ import logging
 
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-from django.conf import settings
 
-import predictionio
 from hunger.models import InvitationCode
 
 from apps.user_actions.models import Bookmark, Done, View
+from apps.user_actions.tasks import pio_new_event
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
-
-# Get an instance of prediction io event client
-pio_access_key = getattr(settings, 'PIO_ACCESS_KEY', "unknown access key")
-pio_eventserver = getattr(settings, 'PIO_EVENT_SERVER_ENDPOINT', "http://localhost:7070")
-
-event_client = predictionio.EventClient(
-    access_key=pio_access_key,
-    url=pio_eventserver,
-    threads=5, qsize=500)
-
-
-def pio_new_event(event_type, user_action):
-    try:
-        event_client.record_user_action_on_item(event_type, user_action.user.id, user_action.event.id,
-                                                event_time=user_action.created)
-    except predictionio.NotCreatedError:
-        pass
 
 ## POST_SAVES
 @receiver(post_save, sender=View)
 def view_post_save(sender, **kwargs):
     user_action = kwargs.get('instance')
-    pio_new_event('view', user_action)
+    pio_new_event('view', user_action.user.id, user_action.event.id, user_action.created)
 
 
 @receiver(post_save, sender=Done)
 def done_post_save(sender, **kwargs):
     user_action = kwargs.get('instance')
-    pio_new_event('done', user_action)
+    pio_new_event('done', user_action.user.id, user_action.event.id, user_action.created)
 
     code, created = InvitationCode.objects.get_or_create(owner=user_action.user, max_invites=100)
     code.num_invites += 1
@@ -49,7 +31,7 @@ def done_post_save(sender, **kwargs):
 @receiver(post_save, sender=Bookmark)
 def bookmark_post_save(sender, **kwargs):
     user_action = kwargs.get('instance')
-    pio_new_event('bookmark', user_action)
+    pio_new_event('bookmark', user_action.user.id, user_action.event.id, user_action.created)
 
 
 @receiver(post_delete, sender=Bookmark)
--- alltoez/apps/user_actions/tasks.py
+++ alltoez/apps/user_actions/tasks.py
@@ -4,12 +4,24 @@ __author__ = 'devangmundhra'
 
 import logging
 
+from django.conf import settings
+
 from celery import shared_task
+import predictionio
 
 from apps.user_actions.models import View
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
+
+# Get an instance of prediction io event client
+pio_access_key = getattr(settings, 'PIO_ACCESS_KEY', "unknown access key")
+pio_eventserver = getattr(settings, 'PIO_EVENT_SERVER_ENDPOINT', "http://localhost:7070")
+
+event_client = predictionio.EventClient(
+    access_key=pio_access_key,
+    url=pio_eventserver,
+    threads=5, qsize=500)
 
 @shared_task
 def mark_user_views_event(event_id, user_id=None):
@@ -17,3 +29,11 @@ def mark_user_views_event(event_id, user_id=None):
     view.event_id = event_id
     view.user_id = user_id
     view.save()
+
+@shared_task
+def pio_new_event(event_type, user_id, event_id, created):
+    try:
+        event_client.record_user_action_on_item(event_type, user_id, event_id,
+                                                event_time=created)
+    except predictionio.NotCreatedError:
+        pass
