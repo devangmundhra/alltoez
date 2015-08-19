3393646ab82b295e72d3e5989862010c71bc92c5
Devang Mundhra <devangmundhra@gmail.com>
2015-08-18 21:12:10 -0700

    Fix datamigrations

--- apps/alltoez_profile/migrations/0004_auto_20150505_0611.py
+++ apps/alltoez_profile/migrations/0004_auto_20150505_0611.py
@@ -4,33 +4,6 @@ from __future__ import unicode_literals
 from django.db import models, migrations
 import django.contrib.gis.db.models.fields
 
-from apps.alltoez_profile.models import UserProfile
-
-
-def add_last_filter_details(apps, schema_editor):
-    """
-    Add user profile info for last filter detail
-    :param apps:
-    :param schema_editor:
-    :return:
-    """
-    for profile in UserProfile.objects.all():
-        if profile.point and profile.city:
-            profile.last_filter_center = profile.point
-            profile.last_filter_location_name = profile.city
-            profile.last_filter_radius = 7  # 7 miles
-            profile.save()
-
-
-def delete_last_filter_details(apps, schema_editor):
-    """
-    When rolling back, nothing to do
-    :param apps:
-    :param schema_editor:
-    :return:
-    """
-    pass
-
 
 class Migration(migrations.Migration):
 
@@ -57,5 +30,4 @@ class Migration(migrations.Migration):
             field=models.FloatField(null=True, blank=True),
             preserve_default=True,
         ),
-        migrations.RunPython(add_last_filter_details, reverse_code=delete_last_filter_details),
     ]
--- apps/events/migrations/0010_event_published_at.py
+++ apps/events/migrations/0010_event_published_at.py
@@ -3,31 +3,6 @@ from __future__ import unicode_literals
 
 from django.db import models, migrations
 
-from apps.events.models import Event
-
-
-def add_published_dates(apps, schema_editor):
-    """
-    If event is marked as published, call save on it
-    :param apps:
-    :param schema_editor:
-    :return:
-    """
-    for event in Event.objects.all():
-        if event.publish:
-            event.published_at = event.created_at
-            event.save()
-
-
-def delete_published_dates(apps, schema_editor):
-    """
-    When rolling back, nothing to do
-    :param apps:
-    :param schema_editor:
-    :return:
-    """
-    pass
-
 
 class Migration(migrations.Migration):
 
@@ -42,5 +17,4 @@ class Migration(migrations.Migration):
             field=models.DateTimeField(db_index=True, null=True, blank=True),
             preserve_default=True,
         ),
-        migrations.RunPython(add_published_dates, reverse_code=delete_published_dates),
     ]
