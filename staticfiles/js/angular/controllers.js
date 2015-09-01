fa9e9d93b4202a8c54a8ea9608f16565032c65fc
renjithraj <renjithraj2005@gmail.com>
2015-09-01 17:45:30 +0530

    Small refactoring

--- staticfiles/js/angular/controllers.js
+++ staticfiles/js/angular/controllers.js
@@ -6,8 +6,6 @@ app.controller('mainController', function($scope, $location){
 
 });
 
-
-
 app.controller('aboutPageController', function($scope, $location) {
 
     var main_div = angular.element( document.querySelector( '#above-the-fold' ) );
