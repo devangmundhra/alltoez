8c612cfd98b39184faedbf0bbc8c1f3252950423
Devang Mundhra <devangmundhra@gmail.com>
2015-11-02 18:20:37 +0530

    Try removing staticfiles

--- staticfiles/js/angular/app.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module('app', ['ngRoute', ]);
-
-    // HTTP requests need to be considered AJAX requests.
-app.config(["$httpProvider", function($httpProvider) {
-    $httpProvider.defaults.xsrfCookieName = 'csrftoken';
-    $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
-    $httpProvider.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
-    $httpProvider.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
-}]);
--- staticfiles/js/angular/router.js
+++
@@ -1,3 +0,0 @@
-//router
-app.config(['$routeProvider',
-  function($routeProvider) {}]);
